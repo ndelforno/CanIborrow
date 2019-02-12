dc656aa2364835a42be614ac1a85e64d21cbd7b9
Ndelforno <nicolas.delforno@gmail.com>
2019-02-12 16:33:29 -0500

    Create and show method completed

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -8,4 +8,23 @@ class ToolsController < ApplicationController
     @tool = Tool.find_by(params[:id])
   end
 
+  def new
+    @tool = Tool.new
+  end
+
+  def create
+    @tool = Tool.new
+    @tool.name = params[:tool][:name]
+    @tool.description = params[:tool][:description]
+    @tool.price = params[:tool][:price]
+    @tool.address = params[:tool][:address]
+    @tool.user = current_user
+    if @tool.save
+      redirect_to tool_path(@tool)
+    else
+      render :new
+    end
+
+  end
+
 end
