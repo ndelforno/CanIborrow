bdd207b49f8f53dc4d902483d9baec99c862a9c0
Ndelforno <nicolas.delforno@gmail.com>
2019-02-13 15:45:22 -0500

    reservation create method done

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,19 @@
 class ReservationsController < ApplicationController
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new
+    @reservation.user = current_user
+    @reservation.date = params[:reservation][:date]
+    @reservation.reservation_time_start = params[:reservation][:reservation_time_start]
+    @reservation.reservation_time_end = params[:reservation][:reservation_time_end]
+    @tool = Tool.find_(params[:tool_id])
+    @reservation.tool = @tool
+    if @reservation.save
+      redirect_to tool_path(@tool)
+    end
+  end
 end
