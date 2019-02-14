96328cc1074e9526ae6c739f0d618a03563a8ff7
Ndelforno <nicolas.delforno@gmail.com>
2019-02-14 17:27:42 -0500

    map added on index

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,27 +1,11 @@
 document.addEventListener("turbolinks:load", function(event) {
 
-  if(document.getElementById('mapid')){
+  if(document.getElementById('mapindex')){
 
-    var mymap = L.map('mapid');
-    var address = document.getElementById("address");
-    console.log(address);
-    var addressText = address.innerText;
+    var mymap = L.map('mapindex');
 
     mymap.setView([43.6532, -79.3832], 13);
 
-    if(address){
-      axios({
-        url: "https://nominatim.openstreetmap.org/search?format=json&limit=3&q=" + addressText,
-        method: 'get',
-        data: '',
-        dataType: 'json',
-      }).then(function(response){
-        var lat = response.data[0].lat;
-        var long = response.data[0].lon;
-        mymap.setView([lat, long], 13)
-        var marker = L.marker([lat,long]).addTo(mymap);
-      });
-    }
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
