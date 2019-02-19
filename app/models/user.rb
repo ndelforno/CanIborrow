db261a86a5a4062be1bf14db79b37d8adf8974b8
Ndelforno <nicolas.delforno@gmail.com>
2019-02-19 14:22:17 -0500

    User profile setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :first_name, :last_name, presence: true
   validates :password, confirmation: true
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
 end
