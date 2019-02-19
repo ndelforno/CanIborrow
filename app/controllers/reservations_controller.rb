2a3233411dafaccc375c57dfc8aff181fd0d7162
Ndelforno <nicolas.delforno@gmail.com>
2019-02-19 16:15:45 -0500

    added bottstrap form to reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,8 +9,16 @@ class ReservationsController < ApplicationController
     @tool = Tool.find(params[:tool_id])
     @reservation.user = current_user
     @reservation.date = params[:reservation][:date]
-    @reservation.reservation_time_start = params[:reservation][:reservation_time_start]
-    @reservation.reservation_time_end = params[:reservation][:reservation_time_end]
+    @reservation.reservation_time_start = DateTime.new(params[:reservation]["reservation_time_start(1i)"].to_i,
+      params[:reservation]["reservation_time_start(2i)"].to_i,
+      params[:reservation]["reservation_time_start(3i)"].to_i,
+      params[:reservation]["reservation_time_start(4i)"].to_i,
+      params[:reservation]["reservation_time_start(5i)"].to_i)
+    @reservation.reservation_time_end = DateTime.new(params[:reservation]["reservation_time_end(1i)"].to_i,
+      params[:reservation]["reservation_time_end(2i)"].to_i,
+      params[:reservation]["reservation_time_end(3i)"].to_i,
+      params[:reservation]["reservation_time_end(4i)"].to_i,
+      params[:reservation]["reservation_time_end(5i)"].to_i)
     @reservation.tool_id = @tool.id
     if @reservation.save
       redirect_to tool_path(@tool)
