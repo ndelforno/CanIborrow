1677999a11a06af1b6a4d256ddfdf2410d07f472
Ndelforno <nicolas.delforno@gmail.com>
2019-02-20 14:01:54 -0500

    setup bounds for map + dry requests

--- app/assets/javascripts/tools.js
+++ app/assets/javascripts/tools.js
@@ -1,82 +1,80 @@
-document.addEventListener("turbolinks:load", function(event) {
+const fetchOpenStreetMap = (query) => {
+  return axios({
+    url: "https://nominatim.openstreetmap.org/search?format=json&limit=3&q=" + query,
+    method: 'get',
+    data: '',
+    dataType: 'json',
+  });
+};
 
-  if(document.getElementById('mapid')){
+document.addEventListener("turbolinks:load", async (event) => {
+  const hasMap = document.getElementById('mapid');
+  const arrayOfLatLong = []
 
+  if (hasMap) {
     var mymap = L.map('mapid');
     var toolAddress = document.getElementById("address");
-    var toolAddressText = toolAddress.innerText;
-    if (document.getElementById("user_address")) {
-      var userAddress = document.getElementById("user_address")
-      var userAddressText = userAddress.innerText
-    }
-    var arrayOfLatLong = []
+    var toolAddressText = toolAddress.innerText || '';
+    var userAddress = document.getElementById("user_address");
+    var userAddressText = userAddress.innerText || '';
 
     var homeIcon = L.icon({
       iconUrl: "https://image.flaticon.com/icons/png/128/25/25694.png",
       shadowUrl: '',
 
-      iconSize:     [30, 30], // size of the icon
-      shadowSize:   [50, 64], // size of the shadow
-      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-      shadowAnchor: [4, 62],  // the same for the shadow
-      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+      iconSize: [30, 30], // size of the icon
+      shadowSize: [50, 64], // size of the shadow
+      iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+      shadowAnchor: [4, 62], // the same for the shadow
+      popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
 
     mymap.setView([43.6532, -79.3832], 13);
 
 
-    if(toolAddress){
-      axios({
-        url: "https://nominatim.openstreetmap.org/search?format=json&limit=3&q=" + toolAddressText,
-        method: 'get',
-        data: '',
-        dataType: 'json',
-      }).then(function(response){
-        const toolAddressLat = response.data[0].lat;
-        const toolAddressLong = response.data[0].lon;
-        arrayOfLatLong.push([toolAddressLat,toolAddressLong])
-        mymap.setView([toolAddressLat, toolAddressLong], 13)
-        var marker = L.marker([toolAddressLat,toolAddressLong]).addTo(mymap);
-      });
+    if (toolAddress) {
+      const {
+        data
+      } = await fetchOpenStreetMap(toolAddressText);
+      const {
+        lat,
+        lon
+      } = data[0];
+      const corner1 = [lat, lon];
+
+      arrayOfLatLong.push(corner1)
+      mymap.setView(corner1, 13)
+      L.marker(corner1).addTo(mymap);
     }
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoibmRlbGZvcm5vIiwiYSI6ImNqbzY2MzlyZTBoczUzcW5sc2k3dGFsZ2YifQ.JL97VOzlsaPc4uDrUwlAnw'
-      }).addTo(mymap);
-    }
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoibmRlbGZvcm5vIiwiYSI6ImNqbzY2MzlyZTBoczUzcW5sc2k3dGFsZ2YifQ.JL97VOzlsaPc4uDrUwlAnw'
+    }).addTo(mymap);
+  }
 
-    if(userAddress){
-      console.log(userAddress);
-      axios({
-        url: "https://nominatim.openstreetmap.org/search?format=json&limit=3&q=" + userAddressText,
-        method: 'get',
-        data: '',
-        dataType: 'json',
-      }).then(function(response){
-        const userAddressLat = response.data[0].lat;
-        const userAddressLong = response.data[0].lon;
-        var corner1 = [userAddressLat,userAddressLong]
-        arrayOfLatLong.push([userAddressLat,userAddressLong])
-        mymap.setView([userAddressLat, userAddressLong], 13)
-        var marker = L.marker([userAddressLat,userAddressLong],
-        {icon: homeIcon})
-        .addTo(mymap);
-      });
-      
-    var testarray = [];
-    testarray.push([1,1])
-    testarray.push([4,4])
-    console.log(testarray);
+  if (userAddress) {
+    const {
+      data
+    } = await fetchOpenStreetMap(userAddressText);
+    const {
+      lat,
+      lon
+    } = data[0];
+    const corner2 = [lat, lon];
+
+    arrayOfLatLong.push(corner2)
+    mymap.setView(corner2, 13)
+    L.marker(corner2, {
+        icon: homeIcon
+      })
+      .addTo(mymap);
+
     console.log(arrayOfLatLong);
     var bounds = new L.LatLngBounds(arrayOfLatLong);
     console.log(bounds);
     mymap.fitBounds(bounds);
-
-
-
-    }
-
+  }
 });
