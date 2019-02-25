f522929fbd540933db627b0c65c25056c1177915
Ndelforno <nicolas.delforno@gmail.com>
2019-02-25 14:42:01 -0500

    update user form added + partial created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,10 +14,27 @@ class UsersController < ApplicationController
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.save
       session[:user_id] = @user.id
-      redirect_to tools_path, notice: "Logged in as #{@user.email}!"
+      redirect_to request.referer, notice: "Logged in as #{@user.email}!"
     else
       redirect_to request.referer, notice: "signup failed !"
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.first_name = params[:user][:first_name]
+    @user.last_name = params[:user][:last_name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.address = params[:user][:address]
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.save
+      redirect_to user_path(@user)
+    end 
   end
 
   def show
