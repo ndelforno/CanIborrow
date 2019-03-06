07154dcf446e1ddf652d1103b33611262b00c633
Ndelforno <nicolas.delforno@gmail.com>
2019-03-06 12:59:42 -0500

    method done to count days and get total price

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,10 +8,16 @@ class Reservation < ApplicationRecord
 
   def end_date_after_start_date
     return if reservation_time_start.blank? || reservation_time_end.blank?
-    
+
      if reservation_time_end < reservation_time_start
        errors.add(:reservation_time_end, "must be after the start date")
      end
+  end
+
+  def numbers_of_days
+    diff = self.reservation_time_end - self.reservation_time_start
+    days = (diff.to_f) /1.day
+    days.floor
   end
 
 end
