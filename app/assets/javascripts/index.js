c3ccbe3389c12bb31fa04ee595ee673abda118fb
Ndelforno <nicolas.delforno@gmail.com>
2019-03-12 14:53:15 -0400

    display modal for signup

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,21 +1,5 @@
 document.addEventListener("turbolinks:load", function(event) {
   $('#navbarDropdown').dropdown()
 
-//open signin
-  function displayModal() {
-    var modal = document.querySelector('.modal')
-    modal.style.display = "block"
-  };
-  var buttonSignin = document.querySelector('.signin');
-  buttonSignin.addEventListener('click', displayModal)
-
-//close signin
-
-  var buttonClose = document.querySelector('.close')
-  buttonClose.addEventListener('click', closeSignin)
-  function closeSignin(){
-    var modal = document.querySelector('.modal')
-    modal.style.display = "none"
-  }
 
 });
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,11 @@ def current_user
   @current_user ||= User.find(session[:user_id]) if session[:user_id]
 end
 
+def set_new_user
+  @user = User.new
+end
 
-helper_method :current_user
+
+helper_method :current_user, :set_new_user
 
 end
